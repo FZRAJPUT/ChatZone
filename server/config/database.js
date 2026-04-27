f82e19bdeddaa638e8024478141b2854d0e32000
Subhash Kumar <subhashkushwah134@gmail.com>
2026-04-27 21:15:08 +0530

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config()
 
 const connectDB = async () => {
-    await mongoose.connect("mongodb://localhost:27017/chat")
+    await mongoose.connect(process.env.DB_URI)
         .then(() => console.log("Database connected sucsessfully..."))
         .catch((e) => console.log(e.message))
 }
